382070030dc79c86a3598358f93d7a4942715ac9
MISAKI <275684857@qq.com>
2024-06-24 01:24:34 +0800

    Update block.ts

--- src/core/graphflow/block.ts
+++ src/core/graphflow/block.ts
@@ -6,7 +6,19 @@ export class Block extends Ev  {
     }
 
     draw(){
+        let temp = async function(){
+            //...
+        }
 
+        let temp1 = async function(){
+            //...
+        }
+
+        let temp2 = async function(){
+            //...
+        }
+
+        // 多次调用draw, js引擎是否会对函数内创建多个函数的逻辑做优化，比如不需要重新编译
     }
 
     drag(){
@@ -22,7 +34,7 @@ export class Block extends Ev  {
     }
 
     import(){
-        
+
     }
 
     export(){
